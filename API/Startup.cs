1a819a2fb1cb2669e99ebfe68aaa4518263b4c5c
getbin100 <kalget100@gmail.com>
2021-04-26 03:18:16 +0300

    Update Startup.cs

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {

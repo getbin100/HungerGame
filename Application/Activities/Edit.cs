41c103d0f28f8408fb2658e3d2bc8d820927255a
getbin100 <kalget100@gmail.com>
2021-04-28 01:20:14 +0300

    AutoMapper is implemented

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -17,8 +18,10 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
 
             }
@@ -26,7 +29,7 @@ namespace Application.Activities
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
-                activity.Title = request.Activity.Title ?? activity.Title;
+                _mapper.Map(request.Activity, activity);
                 await _context.SaveChangesAsync();
 
                 return Unit.Value;
